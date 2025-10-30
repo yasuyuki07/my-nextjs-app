8590e50dad912e7b2c15f36a5354fe3195a06ecd
yasuyuki07 <46763357+yasuyuki07@users.noreply.github.com>
2025-10-31 05:47:36 +0900

    Update page.tsx

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'
-
 'use client';
+
+export const dynamic = 'force-dynamic'
 
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
